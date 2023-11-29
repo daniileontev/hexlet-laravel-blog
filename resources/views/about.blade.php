d083c62eb04e6065b0b234f81fd02a03c70b46d7
daniil <75467751+daniileontev@users.noreply.github.com>
2023-11-29 15:16:34 +0500

    add layouts

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,17 @@
-<h1>О блоге</h1>
-<p>Эксперименты с Laravel на Хекслете</p>
+@extends('layouts.app')
+
+<!-- Секция, содержимое которой обычный текст. -->
+@section('title', 'О блоге')
+
+<!-- Секция, содержащая HTML блок. Имеет открывающую и закрывающую часть. -->
+@section('content')
+    <h1>О блоге</h1>
+    <p>Эксперименты с Laravel на Хекслете</p>
+
+    @foreach($team as $member)
+        <h2>{{ $member['name'] }}</h2>
+        <p>{{ $member['position'] }}</p>
+    @endforeach
+
+@endsection
+
